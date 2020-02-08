b233ebd6922c49d68c83f0946b689c591a332b7b
Tor Hedin Brønner <torhedinbronner@gmail.com>
2020-02-09 00:09:33 +0100

    glib-networking: 2.62.3 -> 2.63.92

--- pkgs/development/libraries/glib-networking/default.nix
+++ pkgs/development/libraries/glib-networking/default.nix
@@ -18,13 +18,13 @@
 
 stdenv.mkDerivation rec {
   pname = "glib-networking";
-  version = "2.62.3";
+  version = "2.63.92";
 
   outputs = [ "out" "installedTests" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "1xvzixzwwh8wk8f79qbm2gl7hp3hj1a7skv281v5qsx74dpzi8cc";
+    sha256 = "0k9wipnnj4bn5d34isnxragligny5idk6xmmw8vj8xfwxdif0gpa";
   };
 
   patches = [
