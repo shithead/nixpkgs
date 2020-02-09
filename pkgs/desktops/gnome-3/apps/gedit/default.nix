2d47c0fb50ea26bd6f5eb699d8b2691a35eee3d2
Tor Hedin Brønner <torhedinbronner@gmail.com>
2020-02-09 01:31:27 +0100

    gnome3.gedit: 3.34.1 -> 3.35.90
    - No longer depends on libxml
    - tepl is a new dependency  (which should probably propagate amtk)

--- pkgs/desktops/gnome-3/apps/gedit/default.nix
+++ pkgs/desktops/gnome-3/apps/gedit/default.nix
@@ -1,20 +1,22 @@
 { stdenv, meson, fetchurl, python3
 , pkgconfig, gtk3, glib, adwaita-icon-theme
 , libpeas, gtksourceview4, gsettings-desktop-schemas
-, wrapGAppsHook, ninja, libsoup, libxml2
-, gnome3, gspell, perl, itstool, desktop-file-utils }:
+, wrapGAppsHook, ninja, libsoup, tepl
+, gnome3, gspell, perl, itstool, desktop-file-utils
+, amtk
+}:
 
 stdenv.mkDerivation rec {
   pname = "gedit";
-  version = "3.34.1";
+  version = "3.35.90";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gedit/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "1inm50sdfw63by1lf4f1swb59mpyxlly0g5rdg99j5l3357fzygb";
+    sha256 = "19gx4czdhsdl1cyl01rqd2vks5rz2p4gr4ijlaszs118s3lyrjlg";
   };
 
   nativeBuildInputs = [
-    pkgconfig wrapGAppsHook meson ninja libxml2
+    pkgconfig wrapGAppsHook meson ninja
     python3 perl itstool desktop-file-utils
   ];
 
@@ -23,6 +25,7 @@ stdenv.mkDerivation rec {
     adwaita-icon-theme libsoup
     libpeas gtksourceview4
     gsettings-desktop-schemas gspell
+    tepl amtk
   ];
 
   postPatch = ''
